8cfc0ee2ac64c7daf1d34e6f29b95ba6d78bad8f
jimmywang1994 <429744781@qq.com>
2018-09-26 20:50:18 +0800

    第二次提交

---
+++ src/com/ww/ThreadDemo/BlockedJoin01.java
@@ -0,0 +1,26 @@
+package com.ww.ThreadDemo;
+
+/**
+ * 插队线程
+ */
+public class BlockedJoin01 {
+    public static void main(String[] args) {
+        Thread t=new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("lambda..." + i);
+            }
+        });
+        t.start();
+
+        for (int i = 0; i < 100; i++) {
+            if (i == 20) {
+                try {
+                    t.join();//插队 main线程被阻塞了
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("main..." + i);
+        }
+    }
+}
